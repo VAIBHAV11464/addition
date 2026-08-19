3342f7faf2ad365ebeda87e4f7d6e0f92f92e8d3
Neo SITE <neosite@VITUNIVERSITY.LOCAL>
2026-08-19 17:03:25 +0530

    Add Python addition program

--- addition.py
+++ addition.py
@@ -5,3 +5,9 @@ result = a + b
 print("First Number:", a)
 print("Second Number:", b)
 print("Sum:", result)
+
+x = a- b
+print("\n")
+print("First Number:", a)
+print("Second Number:", b)
+print("Diff:", x)
