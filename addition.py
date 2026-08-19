43da982c7ecf93777826f8540db39d263ffaed37
Neo SITE <neosite@VITUNIVERSITY.LOCAL>
2026-08-19 17:12:42 +0530

    New function added

--- addition.py
+++ addition.py
@@ -11,3 +11,9 @@ print("\n")
 print("First Number:", a)
 print("Second Number:", b)
 print("Diff:", x)
+
+y = a*b
+print("\n")
+print("First Number:", a)
+print("Second Number:", b)
+print("Mul:", y)
